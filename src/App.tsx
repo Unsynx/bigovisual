5f22452b198a4a819e30af0d1a6408b73a092c98
Nik C <58793008+Unsynx@users.noreply.github.com>
2024-11-11 19:24:21 -0500

    Rough, spagetti layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { GraphBigO } from './GraphBigO'
 
 
 
@@ -9,7 +18,60 @@ function App() {
 
   return (
     <>
-      <Button onClick={() => setCount((count) => count + 1)}>CLICK ME {count}</Button>
+      <div className='container_side'>
+        <div className='side_left'>
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
+        <div className='side_right'>
+          <div className='graph'>
+            <GraphBigO />
+          </div>
+          <div className='control'>
+            <p>Current Step: {count}</p>
+            <div className='button_row'>
+              <Button onClick={() => setCount((count) => count - 1)}>Previous Step</Button>
+              <Button onClick={() => setCount((count) => count + 1)}>Next Step</Button>
+              <Button onClick={() => setCount(() => 100)}>Fast Forward</Button>
+              <Button onClick={() => setCount(() => 0)}>Reset</Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
